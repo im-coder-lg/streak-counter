2fcff84f059f5f0ca308797a0892ad24d7e4c76d
imcoderlg <imcoderlg@protonmail.com>
2022-03-18 16:20:20 +0530

    increment logic

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,23 @@ function assertStreakExists(streakInLocalStorage: string | null): streakInLocalS
   return streakInLocalStorage !== null && streakInLocalStorage !== "";
 }
 
+function shouldIncrementOrResetStreakCount(
+  currentDate: Date,
+  lastLoginDate: string,
+): 'increment' | undefined {
+  // We get 11/5/2021
+  // so to get 5, we split on / and get the second item
+  const difference =
+    currentDate.getDate() - parseInt(lastLoginDate.split('/')[1])
+  // This means they logged in the day after the currentDate
+  if (difference === 1) {
+    return 'increment'
+  }
+  // Otherwise they logged in after a day, which would
+  // break the streak
+  return undefined
+}
+
 const KEY = "streak";
 export function streakCounter(_localStorage: Storage, date: Date): Streak {
   const streakInLocalStorage = _localStorage.getItem(KEY);
@@ -18,6 +35,19 @@ export function streakCounter(_localStorage: Storage, date: Date): Streak {
   if (assertStreakExists(streakInLocalStorage)) {
     try {
       const streak = JSON.parse(streakInLocalStorage);
+      const Storage = _localStorage;
+      const state = shouldIncrementOrResetStreakCount(date, streak.lastLoginDate);
+      const SHOULD_INCREMENT = state === "increment";
+
+      if (SHOULD_INCREMENT) {
+        const updatedStreak = {
+          ...streak,
+          currentCount: streak.currentCount + 1,
+          lastLoginDate: formattedDate(date),
+        };
+        Storage.setItem(KEY, JSON.stringify(updatedStreak));
+        return updatedStreak
+      }
       return streak;
     } catch (error) {
       console.error("Failed to parse streak from localStorage");
