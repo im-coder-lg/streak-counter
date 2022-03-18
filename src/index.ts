bd5991736673d5f12455669e33b9ddaf9b481486
imcoderlg <imcoderlg@protonmail.com>
2022-03-18 10:20:43 +0530

    Progress: make some more things, make test a bit better than idiotic

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-export function streakCounter(_localStorage: Storage, date: Date) {}
+interface Streak {
+    currentCount: number,
+    startDate: string,
+    lastLoginDate: string
+}
 
+export function streakCounter(_localStorage: Storage, date: Date): Streak {
+    return {
+        currentCount: 0,
+        startDate: "11/11/2021",
+        lastLoginDate: "11/11/2021"
+    }
+}
